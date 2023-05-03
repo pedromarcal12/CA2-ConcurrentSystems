1b8b2a183fd561a122bff841c74bbe8f4992b4dc
pedromarcal12 <henrique.pedro.marcal@gmail.com>
2023-05-03 12:23:46 +0100

    First commit
    Creating the java file

---
+++ CA2/src/main/java/com/mycompany/ca2/CA2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ca2;
+
+/**
+ *
+ * @author henri
+ */
+public class CA2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
